6fe5cf2d92a690f170feb71c29e69ecb8b9d9d1c
927138 <jiyh1541@gmail.com>
2023-09-13 01:28:49 +0900

    0912_exercise

---
+++ jiyh/ch4/Exercise5.java
@@ -0,0 +1,10 @@
+package ch4;
+
+public class Exercise5 {
+    
+    public static void main(String[] args) {
+        short x = 22;
+        byte y = (byte) x;
+        System.out.println(y);
+    }
+}
