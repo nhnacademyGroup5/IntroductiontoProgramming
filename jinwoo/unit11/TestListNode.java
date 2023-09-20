7ac2e5c30c6b7d8d192fdb6a0e51d8fcb459b6b4
devhomh <jinwoo7654@naver.com>
2023-09-20 13:25:41 +0900

    Unit 11.1 / 11.2 과제 업로드

--- jinwoo/unit11/TestListNode.java
+++ jinwoo/unit11/TestListNode.java
@@ -2,19 +2,12 @@ package jinwoo.unit11;
 
 public class TestListNode {
     public static void main(String[] args) {
-        ListNode c = new ListNode("C", null);
-        ListNode b = new ListNode("B", c);
-        ListNode a = new ListNode("A", b);
-        ListNode c2 = new ListNode("C", null);
-        ListNode b2 = new ListNode("B", c);
-        ListNode a2 = new ListNode("A", b);
-
-        // System.out.println(insertAfter(a, "B", "D"));
-        // System.out.println(insertBefore(a, "B", "D"));
-        // System.out.println(modify(a, "B", "D"));
-        // System.out.println(findNode(a, "D"));
-        // System.out.println(a);
-        // System.out.println(a.invert());
-        System.out.println();
+        ListNode a = new ListNode("a");
+        a.add("d");
+        a.insertBefore("d", "b");
+        a.insertBefore("d", "c");
+        System.out.println(a);
+        a.invert();
+        System.out.println(a);
     }
 }
